1cb5eb80b03e66779024ba1aca40edacffce764b
Chris Fitzmorris <cfitz93@gmail.com>
2017-09-10 18:02:11 -0500

    Refactor hamming problem

--- hamming/src/hamming.clj
+++ hamming/src/hamming.clj
@@ -2,6 +2,5 @@
 
 (defn distance [dna-1 dna-2]
   "Calculates the Hamming distance between two DNA strands"
-  (if (not (= (count dna-1) (count dna-2)))
-    nil ; undefined for strands of unequal length
+  (if (= (count dna-1) (count dna-2))
     (count (filter (fn [x] (false? x)) (map = dna-1 dna-2)))))
