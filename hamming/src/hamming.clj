71fbb0fe83609e665eff2404baaf81ec31adbb35
Chris Fitzmorris <cfitz93@gmail.com>
2017-09-10 17:40:45 -0500

    Add comment

--- hamming/src/hamming.clj
+++ hamming/src/hamming.clj
@@ -3,5 +3,5 @@
 (defn distance [dna-1 dna-2]
   "Calculates the Hamming distance between two DNA strands"
   (if (not (= (count dna-1) (count dna-2)))
-    nil
+    nil ; undefined for strands of unequal length
     (count (filter (fn [x] (false? x)) (map = dna-1 dna-2)))))
