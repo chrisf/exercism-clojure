100e278aee50d4f064cd3d14105ce6cb4cbaec12
Chris Fitzmorris <cfitz93@gmail.com>
2017-11-22 13:05:16 -0600

    Refactor

--- hamming/src/hamming.clj
+++ hamming/src/hamming.clj
@@ -3,4 +3,4 @@
 (defn distance [dna-1 dna-2]
   "Calculates the Hamming distance between two DNA strands"
   (if (= (count dna-1) (count dna-2))
-    (count (filter (fn [x] (false? x)) (map = dna-1 dna-2)))))
+    (count (filter not (map = dna-1 dna-2)))))
